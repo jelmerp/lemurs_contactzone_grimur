d8e1e0f9f8a3e0bf617152ee7894fae9448a6886
jelmerp <jelmerpoelstra@gmail.com>
2021-08-10 12:56:11 -0400

    Major scripts update

--- scripts/misc/gphocs2bpp.sh
+++
@@ -1,7 +0,0 @@
-cd /home/jelmer/Dropbox/sc_lemurs/proj/hybridzone/
-
-INPUT_GPHOCS=analyses/gphocs/input/r03.wOutgroups.hz.mur3gri2c.gphocsInput.txt
-INPUT_BPP=analyses/bpp/input/mur3gri2c.txt
-sed 's/.*fa//' $INPUT_GPHOCS > $INPUT_BPP
-
-#rsync -avr jwp37@dcc-slogin-02.oit.duke.edu:/datacommons/yoderlab/users/jelmer/proj/hybridzone/analyses/bpp/input/mur3gri2c.txt /home/jelmer/Dropbox/sc_lemurs/proj/hybridzone/analyses/bpp/input/
